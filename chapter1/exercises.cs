7df0e3a9b62b50ecb2f0c0b132a0e23e8633f1c0
rubenruvalcabac <rubenruvalcabac@hotmail.com>
2020-05-24 11:47:16 -0500

    Chapter1 is permutation

--- chapter1/exercises.cs
+++ chapter1/exercises.cs
@@ -12,6 +12,11 @@ namespace chapter1
             Console.WriteLine($"IsUniqueString a {IsUniqueString("a")}");
             */
 
+            Console.WriteLine($"is 'abc' permutation of 'cab' {IsPermutation("abc", "cab")}");
+            Console.WriteLine($"is 'abc' permutation of 'abc' {IsPermutation("abc", "abc")}");
+            Console.WriteLine($"is 'abc' permutation of 'xyz' {IsPermutation("abc", "xyz")}");
+            Console.WriteLine($"is 'a' permutation of 'xyz' {IsPermutation("a", "xyz")}");
+
         }
 
         private static bool IsUniqueString(string stringToCheck)
@@ -28,6 +33,46 @@ namespace chapter1
                         return false;
 
             return true;
+
+        }
+
+        // Given two strings, write a method to decide if one is a permutation of the other
+        private static bool IsPermutation(string first, string second)
+        {
+            if (string.IsNullOrEmpty(first) || string.IsNullOrEmpty(second))
+                return false;
+
+            if (first.Length != second.Length)
+                return false;
+
+            first = first.ToLower();
+            second = second.ToLower();
+            if (first == second)
+                return false;
+
+            var anyDifferentPosition = false;
+            for (int i = 0; i < first.Length; i++)
+            {
+                var exists = false;
+                var samePos = false;
+                for (int j = 0; j < second.Length; j++)
+                    if (first[i] == second[j])
+                    {
+
+                        exists = true;
+                        samePos = i == j;
+                        break;
+                    }
+
+                if (!exists)
+                    return false;
+
+                if (!anyDifferentPosition && !samePos)
+                    anyDifferentPosition = true;
+
+            }
+
+            return anyDifferentPosition;
 
         }
     }
